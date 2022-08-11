044e9ea33b05a16e3e6902551fe83c1259f9ea67
dhey <dhey@groupon.com>
2022-08-11 14:32:06 -0500

    update: next.config

---
+++ apps/web/layout.js
@@ -0,0 +1,219 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "../../packages/ui/Layout.tsx":
+/*!************************************!*\
+  !*** ../../packages/ui/Layout.tsx ***!
+  \************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Layout": () => (/* binding */ Layout)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _header_SearchHeader__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./header/SearchHeader */ "../../packages/ui/header/SearchHeader.tsx");
+/* harmony import */ var _header_LiteHeader__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./header/LiteHeader */ "../../packages/ui/header/LiteHeader.tsx");
+
+
+
+const Layout = ({ children, layoutOptions }) => {
+  if (!layoutOptions) {
+    return /* @__PURE__ */ react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null, children);
+  }
+  const Header = layoutOptions.header.type === "search" ? _header_SearchHeader__WEBPACK_IMPORTED_MODULE_1__.SearchHeader : _header_LiteHeader__WEBPACK_IMPORTED_MODULE_2__.LiteHeader;
+  const Footer = () => /* @__PURE__ */ react__WEBPACK_IMPORTED_MODULE_0___default().createElement("footer", {
+    id: "layout-footer"
+  }, "Footer");
+  return /* @__PURE__ */ react__WEBPACK_IMPORTED_MODULE_0___default().createElement((react__WEBPACK_IMPORTED_MODULE_0___default().Fragment), null, /* @__PURE__ */ react__WEBPACK_IMPORTED_MODULE_0___default().createElement(Header, {
+    layoutOptions
+  }), children, /* @__PURE__ */ react__WEBPACK_IMPORTED_MODULE_0___default().createElement(Footer, null));
+};
+
+
+/***/ }),
+
+/***/ "../../packages/ui/header/LiteHeader.tsx":
+/*!***********************************************!*\
+  !*** ../../packages/ui/header/LiteHeader.tsx ***!
+  \***********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "LiteHeader": () => (/* binding */ LiteHeader)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+const LiteHeader = ({ layoutOptions }) => {
+  const { brand } = layoutOptions;
+  return /* @__PURE__ */ react__WEBPACK_IMPORTED_MODULE_0___default().createElement("header", {
+    id: "lite-header",
+    "data-brand": brand
+  }, /* @__PURE__ */ react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, "Lite Header"));
+};
+
+
+/***/ }),
+
+/***/ "../../packages/ui/header/SearchHeader.tsx":
+/*!*************************************************!*\
+  !*** ../../packages/ui/header/SearchHeader.tsx ***!
+  \*************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "SearchHeader": () => (/* binding */ SearchHeader)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+const SearchHeader = ({ layoutOptions }) => {
+  const { brand } = layoutOptions;
+  return /* @__PURE__ */ react__WEBPACK_IMPORTED_MODULE_0___default().createElement("header", {
+    id: "search-header",
+    "data-brand": brand
+  }, /* @__PURE__ */ react__WEBPACK_IMPORTED_MODULE_0___default().createElement("div", null, "Search Header"));
+};
+
+
+/***/ }),
+
+/***/ "../../packages/ui/index.tsx":
+/*!***********************************!*\
+  !*** ../../packages/ui/index.tsx ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Layout": () => (/* reexport safe */ _Layout__WEBPACK_IMPORTED_MODULE_1__.Layout)
+/* harmony export */ });
+/* harmony import */ var _types__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./types */ "../../packages/ui/types.ts");
+/* harmony import */ var _types__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_types__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony reexport (unknown) */ var __WEBPACK_REEXPORT_OBJECT__ = {};
+/* harmony reexport (unknown) */ for(const __WEBPACK_IMPORT_KEY__ in _types__WEBPACK_IMPORTED_MODULE_0__) if(__WEBPACK_IMPORT_KEY__ !== "default") __WEBPACK_REEXPORT_OBJECT__[__WEBPACK_IMPORT_KEY__] = () => _types__WEBPACK_IMPORTED_MODULE_0__[__WEBPACK_IMPORT_KEY__]
+/* harmony reexport (unknown) */ __webpack_require__.d(__webpack_exports__, __WEBPACK_REEXPORT_OBJECT__);
+/* harmony import */ var _Layout__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./Layout */ "../../packages/ui/Layout.tsx");
+
+
+
+
+/***/ }),
+
+/***/ "../../packages/ui/types.ts":
+/*!**********************************!*\
+  !*** ../../packages/ui/types.ts ***!
+  \**********************************/
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ "react":
+/*!************************!*\
+  !*** external "react" ***!
+  \************************/
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var ui__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ui */ "../../packages/ui/index.tsx");
+/* harmony reexport (unknown) */ var __WEBPACK_REEXPORT_OBJECT__ = {};
+/* harmony reexport (unknown) */ for(const __WEBPACK_IMPORT_KEY__ in ui__WEBPACK_IMPORTED_MODULE_0__) if(__WEBPACK_IMPORT_KEY__ !== "default") __WEBPACK_REEXPORT_OBJECT__[__WEBPACK_IMPORT_KEY__] = () => ui__WEBPACK_IMPORTED_MODULE_0__[__WEBPACK_IMPORT_KEY__]
+/* harmony reexport (unknown) */ __webpack_require__.d(__webpack_exports__, __WEBPACK_REEXPORT_OBJECT__);
+
+
+})();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
+//# sourceMappingURL=layout.js.map
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,26 @@
 const withTM = require("next-transpile-modules")(["ui"]);
+const { writeFile } = require("fs").promises;
 
-const withLayoutConfig = (itaNextConfig) => {
-  let UI = require("../../bundles/layout/dist/layout.js");
+async function download(url, path) {
+  const response = await fetch(url);
+  const buffer = await response.buffer();
+  await writeFile(path, buffer);
+}
+
+const withLayoutConfig = async (itaNextConfig) => {
+  const isProd = process.env.NODE_ENV === "production";
+
+  const bundleName = "layout.js";
+  let bundlePath;
+
+  if (isProd) {
+    bundlePath = `./${bundleName}`;
+    await download(`${process.env.CDN_DIR}/${bundleName}`, bundleName);
+  } else {
+    bundlePath = `../../bundles/layout/dist/${bundleName}`;
+  }
+
+  let UI = require(bundlePath);
 
   const nextConfig = {
     ...itaNextConfig,
@@ -16,11 +35,12 @@ const withLayoutConfig = (itaNextConfig) => {
     },
   };
 
-  setInterval(() => {
-    delete require.cache[
-      require.resolve("../../bundles/layout/dist/layout.js")
-    ];
-    UI = require("../../bundles/layout/dist/layout.js");
+  setInterval(async () => {
+    if (isProd) {
+      await download(`${process.env.CDN_DIR}/${bundleName}`, bundleName);
+    }
+    delete require.cache[require.resolve(bundlePath)];
+    UI = require(bundlePath);
   }, 1000);
 
   return nextConfig;
